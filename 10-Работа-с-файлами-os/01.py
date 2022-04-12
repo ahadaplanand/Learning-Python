3b744addea4c2bb2f710566fd86cc6e4d5e2f36d
Ivan Velichko <81852872+ahadaplanand@users.noreply.github.com>
2022-04-12 18:44:51 +0300

    Delete 10-Работа-с-файлами-os directory

--- 10-Работа-с-файлами-os/01.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def file_info(path): 
-    llist = ['st_uid', 'st_gid', 'st_mtime']
-    
-
-file_info('D:\Study\Программирование на Python\Простой Python. \
-    Современный стиль программирования.pdf')
