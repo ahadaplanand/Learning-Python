2ff4b1a4f68b57bb22708143526fe047abe5c005
Ivan Velichko <81852872+ahadaplanand@users.noreply.github.com>
2022-04-12 18:44:28 +0300

    Delete 08-Диаграмма+процессы directory

--- 08-Диаграмма+процессы/01.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as pt
-
-pt.pie([5, 3, 2], colors=['red', 'green', 'blue'], startangle=90, autopct='%1.2f%%')
-pt.title('Graph')
-pt.legend(['First', 'Second', 'Third'])
-pt.show()
--- 08-Диаграмма+процессы/03.py
+++
@@ -1,9 +0,0 @@
-class Queue:
-    queue = []
-    def push(self, item):
-        self.queue.append(item)
-    def get(self):
-        if self.queue:
-            return self.queue.pop(0)
-        else:
-            return 'error'
