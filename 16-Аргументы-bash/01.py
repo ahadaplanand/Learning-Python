6c7b466865d16f529530e269c37708067dc1bfd4
Ivan Velichko <81852872+ahadaplanand@users.noreply.github.com>
2022-04-12 18:46:24 +0300

    Delete 16-Аргументы-bash directory

--- 16-Аргументы-bash/01.py
+++
@@ -1,9 +0,0 @@
-import sys, argparse
-
-def main():
-    if '--source' in sys.argv:
-        source_index = sys.argv.index('--source') + 1
-        if source_index < len(sys.argv):
-            return sys.argv[source_index]
-
-print(main())
--- 16-Аргументы-bash/02.py
+++
@@ -1,6 +0,0 @@
-import argparse, sys
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--output', '-o', default = 'default')
-args = parser.parse_args(sys.argv[1:])
-print(args.output)
